dcde4cbb484580832becf4d6f4a242a0b177cecd
Arda Şalvarlılar <arda4salvar7@gmail.com>
2024-01-09 23:10:12 +0300

    patlama

---
+++ script.js
@@ -0,0 +1,102 @@
+const wordEl = document.getElementById('word')
+const wrongLettersEl = document.getElementById('wrong-letters')
+const playAgainBtn = document.getElementById('play-button')
+const popup = document.getElementById('popup-container')
+const notification = document.getElementById('notification-container')
+const finalMessage = document.getElementById('final-message')
+
+const figureParts = document.querySelectorAll('.figure-part')
+
+const words = ['application','programming','interface','wizard','nodejs']
+let selected_word = words[Math.floor(Math.random() * words.length)]
+
+const correctLetters = []
+const wrongLetters = []
+
+function displayWord(){
+  wordEl.innerHTML = `
+    ${selected_word
+      .split('')
+      .map(
+        letter => `
+          <span class="letter">
+            ${correctLetters.includes(letter) ? letter : ''}
+          </span>
+      `).join('')
+    }
+  `
+  const innerWord = wordEl.innerText.replace(/\n/g,'')
+  
+  if(innerWord === selected_word){
+    finalMessage.innerText='Congratulations! You won!'
+    popup.style.display = 'flex';
+  }
+}
+
+function gameOver(){
+  finalMessage.innerText = 'You lost.'
+  popup.style.display = 'flex'
+  window.removeEventListener('keydown',detactKeys)
+}
+
+function updateWrongLettersEl(){
+  wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>': ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  `
+  figureParts.forEach((part,index) => {
+    const errors = wrongLetters.length
+    if(index < errors){
+      part.style.display= 'block'
+    }else{
+      part.style.display= 'none'
+    }
+  })
+
+  if(wrongLetters.length == figureParts.length){
+    gameOver()
+  }
+}
+
+function showNotification(){
+  notification.classList.add('show')
+  setTimeout(() => {
+    notification.classList.remove('show')
+
+  },2000)
+}
+
+function detactKeys(e){
+  if(e.keyCode >= 65 && e.keyCode <= 90){
+    const letter = e.key
+
+    if(selected_word.includes(letter)){
+      if(!correctLetters.includes(letter)){
+        correctLetters.push(letter)
+        displayWord()
+      }else{
+        showNotification()
+      }
+    }else{
+      if(!wrongLetters.includes(letter)){
+        wrongLetters.push(letter)
+
+        updateWrongLettersEl()
+      }else{
+        showNotification()
+      }
+    }
+  }
+}
+
+playAgainBtn.addEventListener('click',() => {
+  correctLetters.splice(0)
+  wrongLetters.splice(0)
+  selected_word = words[Math.floor(Math.random() * words.length)]
+  displayWord()
+  updateWrongLettersEl()
+  popup.style.display= 'none'
+})
+
+window.addEventListener('keydown',detactKeys)
+displayWord()
